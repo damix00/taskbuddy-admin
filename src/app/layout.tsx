09fec182c5f77769ee335ca03fbbc8bbb43409b6
Damjan Pavličević <damix4921@gmail.com>
2024-02-27 00:28:46 +0100

    initialized project

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "TaskBuddy",
+    description: "TaskBuddy admin dashboard",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${montserrat.className} dark`}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
+}

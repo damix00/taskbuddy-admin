3c4744b6bcfc4930c67e4c57c4c7acee25870cd9
Damjan Pavličević <damix4921@gmail.com>
2024-03-23 18:50:44 +0100

    Delete user function

--- src/actions/management/user/access.ts
+++ src/actions/management/user/access.ts
@@ -63,3 +63,113 @@ export async function updateLimitedAccess({
         return false;
     }
 }
+
+export async function deleteAccount({
+    userId,
+}: {
+    userId: number;
+}): Promise<boolean> {
+    try {
+        const user = await db.users.findUnique({
+            where: {
+                id: userId,
+            },
+        });
+
+        if (!user) {
+            return false;
+        }
+
+        if (user.role === "admin") {
+            return false;
+        }
+
+        // Delete reviews
+        await db.reviews.deleteMany({
+            where: {
+                user_id: userId,
+                OR: [
+                    {
+                        rating_for_id: userId,
+                    },
+                ],
+            },
+        });
+
+        // Delete interests
+        await db.user_interests.deleteMany({
+            where: {
+                user_id: userId,
+            },
+        });
+
+        // Delete reports
+        await db.user_reports.deleteMany({
+            where: {
+                user_id: userId,
+                OR: [
+                    {
+                        AND: [
+                            {
+                                content_type: 2,
+                            },
+                            {
+                                content_id: userId,
+                            },
+                        ],
+                    },
+                ],
+            },
+        });
+
+        // Delete channel completions
+        await db.job_completions.deleteMany({
+            where: {
+                completed_by_id: userId,
+                OR: [
+                    {
+                        completed_for_id: userId,
+                    },
+                ],
+            },
+        });
+
+        // Delete channels and messages
+        await db.channels.deleteMany({
+            where: {
+                created_by_id: userId,
+                OR: [
+                    {
+                        recipient_id: userId,
+                    },
+                ],
+            },
+        });
+
+        // Delete all posts
+        await db.posts.deleteMany({
+            where: {
+                user_id: userId,
+            },
+        });
+
+        // Delete all logins
+        await db.logins.deleteMany({
+            where: {
+                user_id: userId,
+            },
+        });
+
+        // Permanent delete the user
+        await db.users.delete({
+            where: {
+                id: userId,
+            },
+        });
+
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
---
+++ src/components/user_page/manage/access/DeleteAccount.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { deleteAccount } from "@/actions/management/user/access";
+import {
+    AlertDialog,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
+import { UserContext } from "@/context/user_context";
+import { useContext, useState } from "react";
+
+export default function DeleteAccount() {
+    const [checked, setChecked] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const context = useContext(UserContext);
+    const { toast } = useToast();
+
+    return (
+        <AlertDialog
+            open={open}
+            onOpenChange={(v) => {
+                setOpen(v);
+                if (!v) {
+                    setChecked(false);
+                }
+            }}>
+            <AlertDialogTrigger>
+                <Button variant="destructive">Delete account</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Delete account</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Are you absolutely sure you want to delete this account?
+                        This action cannot be undone.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <div className="flex flex-row items-center gap-2">
+                    <Checkbox
+                        id="delete-acc-checkbox"
+                        checked={checked}
+                        onCheckedChange={(v) => {
+                            setChecked(v as any as boolean);
+                        }}
+                    />
+                    <Label htmlFor="delete-acc-checkbox">
+                        I understand the consequences of deleting this account.
+                    </Label>
+                </div>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <Button
+                        disabled={!checked || loading}
+                        variant="destructive"
+                        onClick={async () => {
+                            setLoading(true);
+
+                            const r = await deleteAccount({
+                                userId: context!.user!.user.id,
+                            });
+
+                            setLoading(false);
+                            setOpen(false);
+
+                            if (!r) {
+                                toast({
+                                    title: "Failed to delete account",
+                                    description:
+                                        "An error occurred while deleting the account.",
+                                    variant: "destructive",
+                                });
+                                return;
+                            }
+
+                            toast({
+                                title: "Account deleted",
+                                description: "The account has been deleted.",
+                            });
+                        }}>
+                        Delete
+                    </Button>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+}

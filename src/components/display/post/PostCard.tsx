d36271c3a8fafe6eae482fcb3a3ade065813cc02
Damjan Pavličević <damix4921@gmail.com>
2024-03-23 13:56:11 +0100

    show interactions on post card

--- src/components/display/post/PostCard.tsx
+++ src/components/display/post/PostCard.tsx
@@ -7,11 +7,22 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Separator } from "@/components/ui/separator";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Post } from "@/components/user_page/posts/types";
-import { timeAgo } from "@/utils/utils";
+import { bigintToInt, timeAgo } from "@/utils/utils";
+import { Bookmark, Eye, Heart, MessageSquare, Share } from "lucide-react";
 import Link from "next/link";
+
+function Interaction({ icon, count }: { icon: any; count: any }) {
+    return (
+        <div className="flex gap-1 flex-col items-center">
+            {icon}
+            <div className="text-xs text-muted-foreground">
+                {bigintToInt(count)}
+            </div>
+        </div>
+    );
+}
 
 export default function PostCard({ post }: { post: Post }) {
     return (
@@ -47,7 +58,36 @@ export default function PostCard({ post }: { post: Post }) {
                 <CardContent className="gap-2 flex flex-col">
                     <img className="rounded-md" src={post.media[0].media} />
                 </CardContent>
-                <Separator />
+                <CardFooter>
+                    <ScrollArea className="w-full">
+                        <ScrollBar
+                            orientation="horizontal"
+                            className="w-full"
+                        />
+                        <div className="flex justify-between w-full gap-2">
+                            <Interaction
+                                icon={<Heart className="w-4 h-4" />}
+                                count={post.post_interactions.likes}
+                            />
+                            <Interaction
+                                icon={<MessageSquare className="w-4 h-4" />}
+                                count={post.post_interactions.comments}
+                            />
+                            <Interaction
+                                icon={<Bookmark className="w-4 h-4" />}
+                                count={post.post_interactions.bookmarks}
+                            />
+                            <Interaction
+                                icon={<Share className="w-4 h-4" />}
+                                count={post.post_interactions.shares}
+                            />
+                            <Interaction
+                                icon={<Eye className="w-4 h-4" />}
+                                count={post.post_interactions.impressions}
+                            />
+                        </div>
+                    </ScrollArea>
+                </CardFooter>
             </Card>
         </Link>
     );
